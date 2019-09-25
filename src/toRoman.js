078e089aad1e0cfdc35b12fb5c646daf745da059
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-25 14:31:17 +0200

    feat: added alert to appear after passing arabic number lesser than 0 or higher than 3999

--- src/toRoman.js
+++ src/toRoman.js
@@ -2,9 +2,10 @@ import { convertArabic } from './convertArabic';
 
 const toRoman = (arabic) => {
     let roman = null;
-    if (arabic < 4000) {
+    if (arabic > 0 && arabic < 4000) {
         roman = convertArabic(arabic);
     }
+    else alert("the converter supports numbers from 1 to 3999");
     return roman;
 }
 
