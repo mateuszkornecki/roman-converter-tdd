38d69070fad48f21098855e457c2c3472718d553
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-25 15:16:38 +0200

    Updates

---
+++ precache-manifest.9e7f5d56e6a3b96abe4f4bc7a8d246c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32a6b2e1f824bb40572f5d6af8c50fa",
+    "url": "/roman-converter-tdd/index.html"
+  },
+  {
+    "revision": "71e5f9a39054c9b9921c",
+    "url": "/roman-converter-tdd/static/css/main.f20e0042.chunk.css"
+  },
+  {
+    "revision": "11e5531915d64cfbbacd",
+    "url": "/roman-converter-tdd/static/js/2.7a247ad1.chunk.js"
+  },
+  {
+    "revision": "71e5f9a39054c9b9921c",
+    "url": "/roman-converter-tdd/static/js/main.a9499397.chunk.js"
+  },
+  {
+    "revision": "338571c82c7ca49f8503",
+    "url": "/roman-converter-tdd/static/js/runtime-main.8522d48b.js"
+  }
+]);
