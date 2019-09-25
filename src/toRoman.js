30a4c1a2692d14fb8874c33cd7dad0fa32ac4f45
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-25 10:32:57 +0200

    feat: added convertion of 4 to IV

--- src/toRoman.js
+++ src/toRoman.js
@@ -1,19 +1,26 @@
 
 const convertionForLesserThanFour = (arabic) => {
     const roman = [];
-    for (let i = 0; arabic - i > 0; i++) {
-        roman.push("I");
-    }
+    if (arabic > 0) {
+        for (let i = 0; arabic - i > 0; i++) {
+            roman.push("I");
+        }
+        return roman.join('');
+    } else return null;
+}
+
+const convertionForHigherThanThreeLesserThanFive = (arabic) => {
+    const roman = [convertionForLesserThanFour(5 - arabic), "V"];
     return roman.join('');
 }
 
 const toRoman = (arabic) => {
     let roman = null;
-    if (arabic > 0 && arabic <= 3) {
+    if (arabic <= 3) {
         roman = convertionForLesserThanFour(arabic);
     }
-    else if (arabic == 5) {
-        roman = "V"
+    else if (arabic <= 5) {
+        roman = convertionForHigherThanThreeLesserThanFive(arabic);
     } else if (arabic == 10) {
         roman = "X"
     }
