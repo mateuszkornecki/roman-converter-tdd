1d383f5f2500a85bb0d0b5e0ca94953fb13e99a6
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-26 20:21:50 +0200

    refactor: simplified roman arrays to contain only 2 elements

--- src/convertArabic.js
+++ src/convertArabic.js
@@ -22,25 +22,25 @@ const convertArabic = (arabic) => {
             roman = ["XX", convertArabic(arabic - 20)];
             break;
         case arabic >= 40 && arabic < 50:
-            roman = ["X", "L", convertArabic(arabic + 10 - 50)];
+            roman = ["XL", convertArabic(arabic + 10 - 50)];
             break;
         case arabic >= 50 && arabic < 90:
             roman = ["L", convertArabic(arabic - 50)];
             break;
         case arabic >= 90 && arabic < 100:
-            roman = ["X", "C", convertArabic(arabic + 10 - 100)];
+            roman = ["XC", convertArabic(arabic + 10 - 100)];
             break;
         case arabic >= 100 && arabic < 400:
             roman = ["C", convertArabic(arabic - 100)];
             break;
         case arabic >= 400 && arabic < 500:
-            roman = ["C", "D", convertArabic(arabic + 100 - 500)];
+            roman = ["CD", convertArabic(arabic + 100 - 500)];
             break;
         case arabic >= 500 && arabic < 900:
             roman = ["D", convertArabic(arabic - 500)];
             break;
         case arabic >= 900 && arabic < 1000:
-            roman = ["C", "M", convertArabic(arabic + 100 - 1000)];
+            roman = ["CM", convertArabic(arabic + 100 - 1000)];
             break;
         case arabic >= 1000 && arabic < 4000:
             roman = ["M", convertArabic(arabic - 1000)];
